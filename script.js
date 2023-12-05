936c6c4ed5d251d8766d5b87f45e8fc0bda25c58
melketesfay <melketesfay@gmail.com>
2023-12-05 23:30:40 +0100

    added hour counter, a set and reset button

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 function setValues() {
   var sec = document.querySelector(".secInput input").value;
   var min = document.querySelector(".minInput input").value;
+  var hr = document.querySelector(".hourInput input").value;
 
   function secondCounter() {
+    //set Seconds
     let secContent = document.querySelector(".sec");
     secContent.innerHTML = sec;
+
+    //set Minutes
     let minContent = document.querySelector(".min");
     minContent.innerHTML = min;
+
+    //set Hours
+
+    let hrContent = document.querySelector(".hour");
+    hrContent.innerHTML = hr;
     if (sec > 0) {
-      secContent.innerHTML = sec;
       --sec;
     } else if (min > 0) {
       sec = 59;
 
       --min;
+    } else if (hr > 0) {
+      min = 59;
+
+      --hr;
+    }
+    if (hr == 0 && min == 0) {
+      document.querySelector(".container").style.color = "red";
     }
   }
 
   second = setInterval(secondCounter, 1000);
+
+  return second;
 }
 
-clearInterval(second);
+function reLoad() {
+  clearInterval(setValues().second);
+  this.location.reload();
+}
